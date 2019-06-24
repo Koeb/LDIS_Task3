61a158bfefba13c7271028b352b3c27885431d21
Philipp Lehninger <philipp.lehninger@gmail.com>
2019-06-24 17:03:23 +0200

    try#2 for user demo patch

--- amba_thermometer_nexys_ddr4/synth.tcl
+++ amba_thermometer_nexys_ddr4/synth.tcl
@@ -6,6 +6,11 @@ create_project -part xc7a100t -force amba_thermometer ./synth/build/
 #
 # -----------------------------------------------------------------------------
 #
+read_vhdl synth/TWICtl.vhd
+read_vhdl synth/TempSensorCtl.vhd
+#
+# -----------------------------------------------------------------------------
+#
 read_vhdl src/AmbaDemux4.vhd
 read_vhdl src/ambaMaster.vhd
 read_vhdl src/ambaSlave.vhd
@@ -16,11 +21,7 @@ read_vhdl src/moving_average.vhd
 read_vhdl src/OutputSlave.vhd
 read_vhdl src/parsing7seg.vhd
 read_vhdl src/SensorSlave.vhd
-#read_vhdl src/TempSensorCtl.vhdl
-read_vhdl synth/TempSensorCtl.vhd
 read_vhdl src/thermometer.vhdl
-#read_vhdl src/TWICtl.vhdl
-read_vhdl synth/TWICtl.vhd
 read_vhdl src/whole7segment.vhdl
 read_vhdl src/windowsize.vhd
 #
