798b2c157e16327e32bf3dc473d57cec18d1f899
Philipp Lehninger <philipp.lehninger@gmail.com>
2019-06-24 13:33:00 +0200

    synth script is now working, added prog script, added gitignore

--- amba_thermometer_nexys_ddr4/synth.tcl
+++ amba_thermometer_nexys_ddr4/synth.tcl
@@ -6,7 +6,24 @@ create_project -part xc7a100t -force amba_thermometer ./synth/build/
 #
 # -----------------------------------------------------------------------------
 #
-read_vhdl src/*
+read_vhdl src/AmbaDemux4.vhd
+read_vhdl src/ambaMaster.vhd
+read_vhdl src/ambaSlave.vhd
+read_vhdl src/clkdivide.vhdl
+read_vhdl src/DSPSlave.vhd
+read_vhdl src/InputSlave.vhd
+read_vhdl src/moving_average.vhd
+read_vhdl src/OutputSlave.vhd
+read_vhdl src/parsing7seg.vhd
+read_vhdl src/SensorSlave.vhd
+read_vhdl src/TempSensorCtl.vhdl
+read_vhdl src/thermometer.vhdl
+read_vhdl src/TWICtl.vhdl
+read_vhdl src/whole7segment.vhdl
+read_vhdl src/windowsize.vhd
+#
+# -----------------------------------------------------------------------------
+#
 read_xdc  synth/constraints.xdc
 #
 # -----------------------------------------------------------------------------
